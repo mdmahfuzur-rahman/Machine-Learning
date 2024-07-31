8a63170525d8d1a9f1dccf4a57a4320f3c7af07b
MD Mahfuzur Rahman <129702499+mdmahfuzur-rahman@users.noreply.github.com>
2024-07-31 15:06:18 +0100

    Add files via upload

---
+++ program 15.py
@@ -0,0 +1,14 @@
+#Break and continue
+i = 1
+while i <= 50:
+    if i == 25:
+        continue
+    print(i)
+    i = i + 1
+#Break
+i = 1
+while i <= 50:
+    if i == 25:
+        break
+    print(i)
+    i = i + 1
