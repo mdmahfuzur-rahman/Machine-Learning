bc0eed6df7fff82304ce2605b0c74ac40568522a
MD Mahfuzur Rahman <129702499+mdmahfuzur-rahman@users.noreply.github.com>
2024-08-03 19:13:20 +0100

    Add files via upload

---
+++ program01loop.py
@@ -0,0 +1,7 @@
+#exercise 07:Python program to count the total number of digits in a number.
+given_number = 182746324
+given_number = str(given_number)
+count = 0
+for i in given_number:
+    count+=1
+print(count)
