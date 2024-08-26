55316ecf3a35e54892f6c0d22b088e8c374cc7b4
MD Mahfuzur Rahman <129702499+mdmahfuzur-rahman@users.noreply.github.com>
2024-08-26 20:03:35 +0100

    Add files via upload

---
+++ program02loop.py
@@ -0,0 +1,20 @@
+girls = ["Alice", "Bella", "Clara", "Diana"]
+
+for x in girls:
+    print(f"Will you marry me, {x}?")
+
+print("----------------------------------------------------------------------")
+
+girls = ["Alice", "Bella", "Clara", "Diana"]
+proposals = [
+    "You light up my life, Alice. Will you marry me?",
+    "Bella, my heart belongs to you. Will you marry me?",
+    "Clara, I can't imagine life without you. Will you marry me?",
+    "Diana, you're my everything. Will you marry me?"
+]
+
+for i in range(len(girls)):
+    print(proposals[i])
+
+
+
